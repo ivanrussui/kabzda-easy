64c5ddd55398dd0d48996c4213a97cc622008804
Ivan <fenomen_ny@mail.ru>
2023-03-15 22:22:07 +0300

    03 - props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import { Rating } from './components/Rating';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // debugger
+
+    return (
+        <div className="App">
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={'Menu'}/>
+            <Accordion title={'Submenu'}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
+
+function Hello() {
+    debugger
+    alert('Hello');
+}
+
+// Hello()
 
 export default App;
